2370362d25402600691ede80bc8a6b9796a17b8a
Cynhzam <cyn.herrerazamudio@gmail.com>
2016-10-14 21:44:02 -0500

    subiendo segunda version de ejercicio

--- js/app.js
+++ js/app.js
@@ -9,9 +9,38 @@ window.addEventListener("load", cargarPagina);
 
     function cargarPagina(){
     	lista.addEventListener("click", aparecerLista);
+    	btnGuardar.addEventListener("click", agregarTarjeta);
     }
 
     function aparecerLista(){
     	lista.classList.add("none");
         formulario.classList.remove("none");
+    }
+
+    function agregarTarjeta(e){
+    	e.preventDefault();
+    	formulario.classList.add("none");
+
+    	var padre = this.parentElement.parentElement;
+
+        var contenedorLista = document.createElement("div");
+    	var textoInput = document.createElement("div");
+        var linkTarjeta = document.createElement("div");
+
+        textoInput.textContent = input.value;
+        linkTarjeta.innerText = "Añadir una tarjeta";
+
+        input.value = "";
+
+        textoInput.classList.add("titulo-lista");
+        linkTarjeta.classList.add("link-tarjeta");
+
+        contenedorPadre.appendChild(contenedorLista);
+        padre.appendChild(linkTarjeta);
+        padre.insertBefore(textoInput, padre.children[0]);
+
+        contenedorLista.insertBefore(lista, contenedorLista.children[0]);
+        contenedorLista.insertBefore(formulario, contenedorLista.children[1]);
+
+        
     }
